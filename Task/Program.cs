0a9676dcf2ace15bd6229d9b30391828a1b55f5c
Dasha <dnazarkina@ya.ru>
2022-12-28 21:45:27 +0300

    added count and SymQty = 3 to match conditions

--- Task/Program.cs
+++ Task/Program.cs
@@ -3,15 +3,24 @@
 либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 лучше обойтись исключительно массивами.*/
 Console.WriteLine("Pls enter Qty of arrey's elements: ");
-int size  = int.Parse(Console.ReadLine());
+int size = int.Parse(Console.ReadLine());
 string[] arr1 = new string[size];
 for (int i = 0; i < size; i++){
-    Console.Write("Pls enter array's element: ");
-    string result = Console.ReadLine();
-    arr1[i] = result;  
+   Console.Write("Pls enter array's element: ");
+   string result = Console.ReadLine();
+   arr1[i] = result; 
 }
 Console.WriteLine();
 Console.Write("Entered array is: [");
 Console.Write(string.Join(",", arr1));
 Console.Write("]");
 
+int count = 0;
+int SymQty = 3;
+for (int i = 0; i < arr1.Length; i++){
+   if(arr1[i].Length <= SymQty){
+       count++;
+    }  
+}
+Console.WriteLine();
+Console.Write("Final array is: ");
