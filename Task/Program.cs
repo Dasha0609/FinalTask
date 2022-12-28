581b7acd36f278325b2c2e89e943c56617e6b6a4
Dasha <dnazarkina@ya.ru>
2022-12-28 21:39:32 +0300

    added first part of code for Array1

---
+++ Task/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.*/
+Console.WriteLine("Pls enter Qty of arrey's elements: ");
+int size  = int.Parse(Console.ReadLine());
+string[] arr1 = new string[size];
+for (int i = 0; i < size; i++){
+    Console.Write("Pls enter array's element: ");
+    string result = Console.ReadLine();
+    arr1[i] = result;  
+}
+Console.WriteLine();
+Console.Write("Entered array is: [");
+Console.Write(string.Join(",", arr1));
+Console.Write("]");
+
